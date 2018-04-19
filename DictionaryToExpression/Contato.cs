ad9389e540540c6c166365a5f3c86e3763714106
Anderson de C. Francisco <anderson.cfrancisco@gmail.com>
2018-04-19 10:19:45 -0300

    Adicionar arquivos de projeto.

---
+++ DictionaryToExpression/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryToExpression
+{
+    class Contato
+    {
+        public string Nome{ get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+      
+        public Contato()
+        {
+            Contato contato = new Contato ();
+        }
+    }
+}
---
+++ DictionaryToExpression/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DictionaryToExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                      List<Contato> Contatos = new List<Contato>();
+            Contatos.Add(new Contato() { Nome = "Anderson", Email = "email@1.com.br", CPF = "12345678912" });
+            Contatos.Add(new Contato() { Nome = "Joao", Email = "email@2.com.br", CPF = "12345678912" });
+            Contatos.Add(new Contato() { Nome = "Pedro", Email = "email@3.com.br", CPF = "98765432198" });
+            Contatos.Add(new Contato() { Nome = "Jonas", Email = "email@1.com.br", CPF = "14725836914" });
+
+            var searchTerms = new Dictionary<string, string> { { "Nome", "Joao" },{"CPF","1234578912" } };
+            var searchExpression = GetFilterExpression(searchTerms);
+           // var result = Contatos.Where(searchExpression).toList();
+        }
+
+        Expression<Func<T, bool>> GetFilterExpression<T>(Dictionary<string, object> searchTerms)
+        {
+
+            foreach (var item in searchTerms)
+            {
+                var condition =
+                 item.Key;
+            }
+
+        }
+    }
+
+  
+
+
+}
